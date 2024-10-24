96b41812d23a54e88fc2c344a825f8d7a9eb9c38
TatisVivas <t_vivas@javeriana.edu.co>
2024-10-23 22:05:59 -0500

    aceptar servicio

--- app/src/main/java/com/example/fixer/WorkerActivity.kt
+++ app/src/main/java/com/example/fixer/WorkerActivity.kt
@@ -1,7 +1,9 @@
 package com.example.fixer
 
 import android.content.Intent
+import android.graphics.Color.parseColor
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +21,20 @@ class WorkerActivity : AppCompatActivity() {
         }
         binding.profileButton.setOnClickListener {
             startActivity(Intent(baseContext, ProfileWorkerActivity::class.java))
+        }
+        binding.acceptButton.setOnClickListener {
+            Toast.makeText(this, "respuesta de aceptación enviada", Toast.LENGTH_SHORT).show()
+            binding.acceptButton.isEnabled = false
+            binding.rejectButton.isEnabled = false
+            binding.acceptButton.setBackgroundColor(parseColor("#808080"))
+            binding.rejectButton.setBackgroundColor(parseColor("#808080"))
+        }
+        binding.rejectButton.setOnClickListener {
+            Toast.makeText(this, "respuesta de rechazo enviada", Toast.LENGTH_SHORT).show()
+            binding.acceptButton.isEnabled = false
+            binding.rejectButton.isEnabled = false
+            binding.acceptButton.setBackgroundColor(parseColor("#808080"))
+            binding.rejectButton.setBackgroundColor(parseColor("#808080"))
         }
     }
 }
